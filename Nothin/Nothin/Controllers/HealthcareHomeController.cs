273b0c8ecc02e3c8726db20b85fb9cd64921de5c
Alec Cleofe <acleofe@uiowa.edu>
2021-06-29 10:10:36 -0500

    Starting Adding for website remake

---
+++ Nothin/Nothin/Controllers/HealthcareHomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nothin.Controllers
+{
+    public class HealthcareHomeController : Controller
+    {
+        // GET: HealthcareHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+        public ActionResult Health()
+        {
+            return View();
+        }
+    }
+}
